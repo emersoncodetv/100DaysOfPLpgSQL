a811ab9b79f718fdff7416eb67bcacfed5a703f8
eperdomo <eperdomo@EPERDOMO-CO.co.oracle.com>
2020-03-04 17:21:08 -0500

    R2D47 Veo que hay mas app-cli cuando se instala postgres, createdb y dropdb. Pense que todo se hacia teniendo una conexión a la instancia.

--- Part_1/Chapter01_Getting_Started/basic-queries.sql
+++ Part_1/Chapter01_Getting_Started/basic-queries.sql
@@ -1,4 +1,14 @@
--- Esto es un comentario, sirve para la documentaciÃ³n del cÃ³digo 
+-- Esto es un comentario, sirve para la documentación del código 
+
+-- Para usar postgresql a través de docker 
+-- Con el bash de git e instalado docker windows
+
+-- Insancia una nueva DB
+$ docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 postgres:12.2
+
+$ winpty docker exec -it -u postgres pg-docker bash
+
+$ createdb mydb
 
 
 SELECT 4 * 3;
