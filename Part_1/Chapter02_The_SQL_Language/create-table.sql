c510344e8e6358f4d2cc24e942a8d81f17e2a9d1
emersondevtoo <emersonvolkov@gmail.com>
2020-04-02 06:55:46 -0500

    R2D73 Repasando tipos de datos, y creando tablas para aprender a definirlos con los tipos de datos, estándar o reservados de PostgreSQL.

---
+++ Part_1/Chapter02_The_SQL_Language/create-table.sql
@@ -0,0 +1,17 @@
+CREATE TABLE weather2 ( 
+city VARCHAR(80),
+City VARCHAR(80),
+temperature_low INT,
+temperature_high INT,
+precipitation REAL,
+DATE DATE
+);
+
+SELECT * FROM weather;
+
+CREATE TABLE cities (
+name varchar(80),
+location point
+);
+
+DROP TABLE weather;
